de6ea86c6044d8b64d8b87d06e912975afe0cdbe
MicheleFumi <michele.fumi96@gmail.com>
2025-04-28 16:58:10 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,139 @@
+const fighters = [
+  {
+    name: "Freezer",
+    power: 8000,
+  },
+  {
+    name: "Vegeta",
+    power: 8500,
+  },
+  {
+    name: "Crilin",
+    power: 500,
+  },
+  {
+    name: "Mr Satan",
+    power: 50,
+  },
+  {
+    name: "Junior",
+    power: 6000,
+  },
+  {
+    name: "Goku",
+    power: 9001,
+  },
+  {
+    name: "Tensing",
+    power: 450,
+  },
+  {
+    name: "Videl",
+    power: 300,
+  },
+  {
+    name: "Bulma",
+    power: 20,
+  },
+  {
+    name: "C-18",
+    power: 7800,
+  },
+  {
+    name: "Gohan",
+    power: 8900,
+  },
+  {
+    name: "Trunks",
+    power: 1250,
+  },
+];
+
+const weapons = [
+  {
+    name: "Ventaglio della Musa",
+    power: 15,
+  },
+  {
+    name: "Scouter",
+    power: 30,
+  },
+  {
+    name: "Bastone Roshi",
+    power: 60,
+  },
+  {
+    name: "Fagioli Magici",
+    power: 70,
+  },
+  {
+    name: "Katana di Yajirobei",
+    power: 85,
+  },
+  {
+    name: "Spada del Dragone Azzurro",
+    power: 115,
+  },
+  {
+    name: "Armatura Saiyan",
+    power: 145,
+  },
+  {
+    name: "Cannone da braccio",
+    power: 170,
+  },
+  {
+    name: "Nuvola d'oro",
+    power: 200,
+  },
+  {
+    name: "Bastone Nyoi",
+    power: 220,
+  },
+  {
+    name: "Spada Z",
+    power: 235,
+  },
+  {
+    name: "Orecchini Potara",
+    power: 250,
+  },
+];
+
+/* assegnazione arma */
+
+let weaponsArray = [...weapons];
+let armoredFighters = fighters.map((fighter) => {
+  let randomWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+  weaponsArray = weaponsArray.filter((weapon) => {
+    weapon.name != randomWeapon.name;
+  });
+  return { ...fighter, weapon: randomWeapon };
+});
+console.log("assegnazione arma", armoredFighters);
+
+/* allenamento */
+
+const training = armoredFighters.map((fighter) => {
+  let fighterLevel = fighter.power + fighter.weapon.power;
+  const multiplier = Math.floor(Math.random() * 100) + 1;
+  const probabilityOfMulti = Math.random().toFixed();
+
+  if (probabilityOfMulti == 1) {
+    let levelMultiplied = fighterLevel * multiplier;
+    return { ...fighter, totalLevel: levelMultiplied };
+  } else {
+    return { ...fighter, totalLevel: fighterLevel };
+  }
+});
+console.log("sessione allenamento", training);
+
+/* qualificazioni */
+const qualificationPassed = training.filter(
+  (fighter) => fighter.totalLevel >= 2000
+);
+if (qualificationPassed.length % 2 != 0) {
+  const robot = { name: "Robot", power: 4000 };
+  qualification.push(robot);
+}
+console.log("giocatori qualificati", qualificationPassed);
